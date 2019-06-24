869cba391002ccc5dc5b1021df465c8d2f94a879
Joseph Jauregui <mgp.699@gmail.com>
2019-06-24 00:10:22 -0400

    Make a View's
    Make a View Folder and Create a Demo View for first Install

---
+++ View/MyFirstView/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>BUBAPP MicroPHP</title>
+</head>
+<body>
+
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<center>
+		<h1><?php echo $params->SayHey ?>, Wellcome to Bubapp</h1>
+	</center>
+	
+</body>
+</html>
