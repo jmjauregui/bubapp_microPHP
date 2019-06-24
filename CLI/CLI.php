e99d13dbef71abb42418feea0f463d6aea1e3f42
Joseph Jauregui <mgp.699@gmail.com>
2019-06-24 11:01:29 -0400

    Creacion de archivos
    Ahora el CLI permite la creacion de archivos necesarios para el proyecto

--- CLI/CLI.php
+++ CLI/CLI.php
@@ -1,5 +1,6 @@
 <?php 
 
+define(__ORIGIN_REPO__, 'https://raw.githubusercontent.com/jmjauregui/bubapp_microPHP/master/RequestFiles/');
 
 $prompt = '';
 
@@ -55,17 +56,23 @@ function Create($prompt)
 function Create_Proyect()
 {
 	if (CreateDIR('Core')) {
-		echo "\n | -> [OK] Core";
+			MakeFile('Core/bubaphp.php', file_get_contents(__ORIGIN_REPO__.'Core_Bubaphp.txt'));
+			MakeFile('Core/bubaphpModel.php', file_get_contents(__ORIGIN_REPO__.'Core_BubaphpModels.txt'));
+			MakeFile('Core/Configurations.php', file_get_contents(__ORIGIN_REPO__.'Core_Configuration.txt'));
 	}	
 	if (CreateDIR('Controller')) {
-		echo "\n | -> [OK] Controller";
+
 	}	
 	if (CreateDIR('View')) {
-		echo "\n | -> [OK] View";
 	}	
 	if (CreateDIR('Model')) {
-		echo "\n | -> [OK] Model";
-	}	
+	}
+
+	MakeFile('index.php', file_get_contents(__ORIGIN_REPO__.'index.txt'));
+	MakeFile('.htaccess', file_get_contents(__ORIGIN_REPO__.'htaccess.txt'));
+
+	
+
 	echo "\n";
 
 }
@@ -86,6 +93,7 @@ function CreateDIR($Ruta)
 {
 	if (!is_dir($Ruta)) {
 		mkdir($Ruta, 0700);
+		echo "\n | -> [DIR][OK] ".$Ruta;
 		return true;
 	}else{
 		echo "\e[0;42;10mCritical Error the folder ".$Ruta." already exist!\e[0m\n";
@@ -93,12 +101,14 @@ function CreateDIR($Ruta)
 	}
 }
 
-function MakeFile($FIleType)
+function MakeFile($nameFile, $Content)
 {
-	$myfile = fopen("Core/Bubaphp.php", "w") or die("Unable to open file!");
-	$txt = base64_decode($FIleType);
+	$myfile = fopen($nameFile, "w") or die("Unable to open file!");
+	$txt = base64_decode($Content);
 	fwrite($myfile, $txt);
 	fclose($myfile);
+	echo "\n | ---> [FILE][CREATED] ".$nameFile;	
+
 }
 
 
